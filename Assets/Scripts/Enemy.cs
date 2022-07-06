1ad43c4d242f415f0a516ea42a79530a3f65619a
AlexeiShishkin <kosopetsnm@gmail.com>
2022-07-06 19:28:54 +0300

    Add project files.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Vector2 _direction;
+
+    private void OnEnable()
+    {
+        _direction = Random.insideUnitCircle.normalized;
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * _direction);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Spawner : ObjectPool
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _timeBetweenSpawns;
+
+    private float _cooldown = 0;
+
+    private void Start()
+    {
+        Initialize(_enemyPrefab);
+    }
+
+    private void Update()
+    {
+        _cooldown -= Time.deltaTime;
+
+        if (_cooldown <= 0)
+        {
+            if (TryGetObject(out GameObject enemy))
+            {
+                int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+                SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
+
+                _cooldown = _timeBetweenSpawns;
+            }
+        }
+    }
+
+    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    {
+        enemy.transform.position = spawnPoint;
+        enemy.SetActive(true);
+    }
+}
